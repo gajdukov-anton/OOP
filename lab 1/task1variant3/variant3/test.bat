87005701a5d4105f513d787a9c17ed74c3058bb2
gajdukov-anton <antony12344@yandex.ru>
2018-02-20 21:14:58 +0300

    поправил тесты

--- lab 1/task1variant3/variant3/test.bat
+++ lab 1/task1variant3/variant3/test.bat
@@ -5,45 +5,45 @@ REM запуск программы без параметров
 IF ERRORLEVEL 1 GOTO err
 
 REM запуск программы с одним словом в качестве параметра (и проверяет результат)
-%PROGRAM% input1.txt Moscow > output.txt
+%PROGRAM% input1.txt Moscow > tests\output.txt
 IF NOT ERRORLEVEL 1 GOTO err
-FC /B output.txt 0.txt
+FC /B tests\output.txt tests\0.txt
 IF ERRORLEVEL 1 GOTO err
 
 REM запуск программы с одной фразой в качестве параметра (и проверяет результат)
-%PROGRAM% input1.txt "Who doesn't love music?"  > output.txt
+%PROGRAM% input1.txt "Who doesn't love music?"  > tests\output.txt
 IF ERRORLEVEL 1 GOTO err
-FC /B output.txt 1.txt
+FC /B tests\output.txt tests\1.txt
 IF ERRORLEVEL 1 GOTO err
 
 REM запуск программы с одним повторяющимся словом в качестве параметра (и проверяет результат)
-%PROGRAM% input1.txt are > output.txt
+%PROGRAM% input1.txt are > tests\output.txt
 IF ERRORLEVEL 1 GOTO err
-FC /B output.txt "7 9 11 15 17.txt"
+FC /B tests\output.txt "tests\7 9 11 15 17.txt"
 IF ERRORLEVEL 1 GOTO err
 
 REM заууск программы с одной фразой в качестве параметра (и проверяет результат)
-%PROGRAM% input1.txt "silence or holidays" > output.txt
+%PROGRAM% input1.txt "silence or holidays" > tests\output.txt
 IF ERRORLEVEL 1 GOTO err
-FC /B output.txt 5.txt
+FC /B tests\output.txt 5.txt
 IF ERRORLEVEL 1 GOTO err
 
 REM запууск программы с одной фразой в качестве параметра (и проверяет результат)
-%PROGRAM% input1.txt "Probably it is so" > output.txt
+%PROGRAM% input1.txt "Probably it is so" > tests\output.txt
 IF ERRORLEVEL 1 GOTO err
-FC /B output.txt 19.txt
-IF ERRORLEVEL 1 GOTO err
-
-REM запууск программы с одной конечной фразой в качестве параметра (и проверяет результат)
-%PROGRAM% input1.txt "This is a great way to mix business with pleasure." > output.txt
-IF ERRORLEVEL 1 GOTO err
-FC /B output.txt 21.txt
+FC /B tests\output.txt tests\19.txt
 IF ERRORLEVEL 1 GOTO err
 
 REM запууск программы с одной конечной фразой в качестве параметра (и проверяет результат)
-%PROGRAM% input2.txt "chanson, hip-hop and others. " > output.txt
+%PROGRAM% input1.txt "This is a great way to mix business with pleasure." > tests\output.txt
 IF ERRORLEVEL 1 GOTO err
-FC /B output.txt 5v2.txt
+FC /B tests\output.txt tests\21.txt
+IF ERRORLEVEL 1 GOTO err
+
+REM запууск программы с одной конечной фразой в качестве параметра (и проверяет результат)
+%PROGRAM% input2.txt "chanson, hip-hop and others. " > tests\output.txt
+IF ERRORLEVEL 1 GOTO err
+FC /B tests\output.txt tests\5v2.txt
 IF ERRORLEVEL 1 GOTO err
 
 ECHO Program testing succeeded :-)
