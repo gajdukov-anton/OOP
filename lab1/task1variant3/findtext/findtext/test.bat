83ab1b6689ce74a52111638615329cd9582c3e25
gajdukov-anton <antony12344@yandex.ru>
2018-02-28 22:25:01 +0300

    Поправил название программ

---
+++ lab1/task1variant3/findtext/findtext/test.bat
@@ -0,0 +1,70 @@
+set PROGRAM="%~1"
+
+REM running program without parametrs
+ECHO Running program without parametrs
+%PROGRAM%
+IF NOT ERRORLEVEL 1 GOTO err
+
+REM running program with empty string
+ECHO Running program with empty string
+%PROGRAM% tests\input1.txt "" > tests\output.txt
+IF NOT ERRORLEVEL 1 GOTO err
+FC /B tests\output.txt tests\emptyString.txt
+IF ERRORLEVEL 1 GOTO err
+
+REM running program with a one word, which not in file
+ECHO Running program with a one word, which not in file
+%PROGRAM% tests\input1.txt Moscow > tests\output.txt
+IF NOT ERRORLEVEL 1 GOTO err
+FC /B tests\output.txt tests\0.txt
+IF ERRORLEVEL 1 GOTO err
+
+REM runnig program with a few words
+ECHO Runnig program with a few words
+%PROGRAM% tests\input1.txt "Who doesn't love music?"  > tests\output.txt
+IF ERRORLEVEL 1 GOTO err
+FC /B tests\output.txt tests\1.txt
+IF ERRORLEVEL 1 GOTO err
+
+REM runnig program with a one repeated words
+ECHO Runnig program with a one repeated words
+%PROGRAM% tests\input1.txt are > tests\output.txt
+IF ERRORLEVEL 1 GOTO err
+FC /B tests\output.txt tests\7_9_11_15_17.txt
+IF ERRORLEVEL 1 GOTO err
+
+REM runnig program with a few words
+ECHO Runnig program with a few words
+%PROGRAM% tests\input1.txt "silence or holidays" > tests\output.txt
+IF ERRORLEVEL 1 GOTO err
+FC /B tests\output.txt tests\5.txt
+IF ERRORLEVEL 1 GOTO err
+
+REM runnig program with a few words
+ECHO Runnig program with a few words
+%PROGRAM% tests\input1.txt "Probably it is so" > tests\output.txt
+IF ERRORLEVEL 1 GOTO err
+FC /B tests\output.txt tests\19.txt
+IF ERRORLEVEL 1 GOTO err
+
+REM runnig program with a few words
+ECHO Runnig program with a few words
+%PROGRAM% tests\input1.txt "This is a great way to mix business with pleasure." > tests\output.txt
+IF ERRORLEVEL 1 GOTO err
+FC /B tests\output.txt tests\21.txt
+IF ERRORLEVEL 1 GOTO err
+
+REM runnig program with a few words
+ECHO Runnig program with a few words
+%PROGRAM% tests\input2.txt "chanson, hip-hop and others. " > tests\output.txt
+IF ERRORLEVEL 1 GOTO err
+FC /B tests\output.txt tests\5v2.txt
+IF ERRORLEVEL 1 GOTO err
+
+ECHO Program testing succeeded :-)
+
+EXIT
+
+:err
+ECHO Program testing failed :-(
+EXIT
