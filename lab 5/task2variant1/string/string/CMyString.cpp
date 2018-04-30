2d5cf3e155f64911e21e19060a6e6f67f874ec63
gajdukov-anton <antony12344@yandex.ru>
2018-04-30 22:26:20 +0300

    добавил  новые операторы

--- lab 5/task2variant1/string/string/CMyString.cpp
+++ lab 5/task2variant1/string/string/CMyString.cpp
@@ -2,6 +2,7 @@
 #include "CMyString.h"
 
 
+
 CMyString::CMyString()
 {
 	m_length = 0;
@@ -12,8 +13,8 @@ CMyString::CMyString(const char * pString, size_t length)
 {
 	m_length = length;
 	m_ptrChar = std::make_unique<char[]>(m_length + 1);
-	memcpy(m_ptrChar.get(), pString, m_length);
-	m_ptrChar[m_length] = (char)(0);
+	memcpy(m_ptrChar.get(), (pString + '\0'), m_length + 1);
+	m_ptrChar[m_length] = '\0';
 }
 
 CMyString::CMyString(const char * pString)
@@ -27,16 +28,13 @@ CMyString::CMyString(CMyString const& other)
 }
 
 CMyString::CMyString(std::string const& stlString)
+	:CMyString(stlString.c_str(), stlString.length())
 {
-	m_length = stlString.length();
-	m_ptrChar = std::make_unique<char[]>(m_length + 1);
-	memcpy(m_ptrChar.get(), stlString.c_str(), m_length);
-	m_ptrChar[m_length] = (char)(0);
 }
 
 const char* CMyString::GetStringData() const
 {
-	return m_ptrChar ? m_ptrChar.get() : "";
+	return m_ptrChar ? m_ptrChar.get() + '\0' : "";
 }
 
 int CMyString::GetLength() const
@@ -88,6 +86,99 @@ bool CMyString::operator ==(CMyString const& other) const
 	return true;
 }
 
+bool CMyString::operator !=(CMyString const& other) const
+{
+	if (other.m_length != m_length)
+	{
+		return true;
+	}
+	for (size_t i = 0; i < m_length; i++)
+	{
+		if (other.m_ptrChar[i] != m_ptrChar[i])
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+CMyString const CMyString::operator +(CMyString const& other) const
+{
+	std::unique_ptr<char[] > newPtrChar = std::make_unique<char[]>(m_length + other.m_length + 1);
+	memcpy(newPtrChar.get(), m_ptrChar.get(), m_length);
+	memcpy(newPtrChar.get() + m_length, other.GetStringData(), other.m_length + 1);
+	return CMyString(newPtrChar.get());
+}
+
+CMyString const operator +(std::string& str, CMyString const& myStr)
+{
+	std::unique_ptr<char[] > newPtrChar = std::make_unique<char[]>(myStr.GetLength() + str.length() + 1);
+	memcpy(newPtrChar.get(), myStr.GetStringData(), myStr.GetLength());
+	memcpy(newPtrChar.get() + myStr.GetLength(), str.c_str(), str.length() + 1);
+	return CMyString(newPtrChar.get());
+}
+
+CMyString const operator +(const char * pString, CMyString const& myStr)
+{
+	std::unique_ptr<char[] > newPtrChar = std::make_unique<char[]>(myStr.GetLength() + strlen(pString) + 1);
+	memcpy(newPtrChar.get(), myStr.GetStringData(), myStr.GetLength());
+	memcpy(newPtrChar.get() + myStr.GetLength(), pString, strlen(pString) + 1);
+	return CMyString(newPtrChar.get());
+}
+
+const char& CMyString::operator[](size_t index) const
+{
+	if (index < m_length && m_length > 0)
+		return m_ptrChar[index];
+	else
+		throw std::out_of_range("the index should be in the range from 0 to the length of the string");
+}
+
+
+char& CMyString::operator[](size_t index)
+{
+	char pr = ' ';
+	if (index < m_length && m_length > 0)
+		return m_ptrChar[index];
+	else
+		throw std::out_of_range("the index should be in the range from 0 to the length of the string");
+}
+
+bool CMyString::operator <(CMyString const& other) const
+{
+	size_t i = 0;
+	while (i < m_length && i < other.m_length)
+	{
+		if (m_ptrChar[i] > other.m_ptrChar[i])
+		{
+			return false;
+		}
+		i++;
+	}
+	if (i < m_length)
+	{
+		return false;
+	}
+	return true;
+}
+
+bool CMyString::operator >(CMyString const& other) const
+{
+	size_t i = 0;
+	while (i < m_length && i < other.m_length)
+	{
+		if (m_ptrChar[i] < other.m_ptrChar[i])
+		{
+			return false;
+		}
+		i++;
+	}
+	if (i < other.m_length)
+	{
+		return false;
+	}
+	return true;
+}
 
 CMyString::~CMyString()
 {
--- lab 5/task2variant1/string/string/CMyString.h
+++ lab 5/task2variant1/string/string/CMyString.h
@@ -2,6 +2,8 @@
 #include <string>
 #include <iostream>
 #include <memory>
+#include <cassert>
+
 
 class CMyString
 {
@@ -14,16 +16,23 @@ public:
 	CMyString& operator =(CMyString const& other);
 	CMyString& operator +=(CMyString const& other);
 	bool operator ==(CMyString const& other) const;
-
+	bool operator !=(CMyString const& other) const;
+	bool operator <(CMyString const& other) const;
+	bool operator >(CMyString const& other) const;
+	CMyString const operator +(CMyString const& other) const;
+	const char & operator[](size_t index) const;
+	char & operator[](size_t index);
 	const char * GetStringData() const;
 	int GetLength() const;
 	void Clear();
 	CMyString SubString(size_t start, size_t length = SIZE_MAX)const;
 
 	~CMyString();
-
 private:
 	std::unique_ptr<char[] > m_ptrChar;
 	size_t m_length = 0;
 };
+
+CMyString const operator +(std::string& str, CMyString const& myStr);
+CMyString const operator +(const char * pString, CMyString const& myStr);
 
